21070e4551a3d0f80437497fb93fe58beb1f50be
Alex Hamon <alex.hmn18@gmail.com>
2024-06-30 19:15:39 +0200

    v1 login with tokenJWT

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  http = inject(HttpClient);
+  service = inject(AuthService);
+  router = inject(Router);
+
+  public loginForm:FormGroup = new FormGroup ({
+    username: new FormControl('', {validators: [Validators.required]}),
+    password: new FormControl('', {validators: [Validators.required]}),  
+  });
+
+  onSubmit(): void {
+
+    const loginData = this.loginForm.getRawValue();
+    this.http.post<any>('http://127.0.0.1:8000/api/login_check', loginData)
+      .subscribe(
+        (response) => {
+          if (response && response.token) {
+            localStorage.setItem('token', response.token);
+            // Vous pouvez stocker d'autres informations utilisateur si nécessaire
+            this.service.currentUser.set(response);
+            this.router.navigateByUrl('');
+            console.log(response);
+          }
+        },
+      );
+  }
+  
+  
+
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
 
 }
