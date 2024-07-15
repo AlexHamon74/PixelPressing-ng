f6b27236f7991c878a6d11eb5b2b740a08a1ab4c
Alex Hamon <alex.hmn18@gmail.com>
2024-07-15 16:30:35 +0200

    update home page dynamic data

--- src/app/features/user/home/home.component.ts
+++ src/app/features/user/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { categoryInterface } from '../../../shared/entities';
+import { CategoryService } from '../../../core/services/category.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  categories: categoryInterface[] = [];
+
+  categoryService = inject(CategoryService);
+
+  ngOnInit(): void {
+      this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.fetchAll().subscribe(categorie => {
+      this.categories = categorie;
+    })
+  }
 }
