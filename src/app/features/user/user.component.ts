0488225d1db1653b465d061c233e3b15c333fdc8
Alex Hamon <alex.hmn18@gmail.com>
2024-07-15 09:00:21 +0200

    update front home page

--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -2,12 +2,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavComponent } from '../../shared/nav/nav.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterLink, NavComponent, RouterOutlet],
-  template: '<app-nav /> <router-outlet />',
+  imports: [RouterLink, NavComponent, RouterOutlet, FooterComponent],
+  template: '<app-nav /> <router-outlet /> <app-footer />',
   styleUrl: 'user.component.css'
 })
 export class UserComponent {
