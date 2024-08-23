7b92e9b80ec6ab8a6b6637cbe8290e753cc5709f
Alex Hamon <alex.hmn18@gmail.com>
2024-08-23 17:50:27 +0200

    update payement page

--- src/app/features/user/payment/payment.component.ts
+++ src/app/features/user/payment/payment.component.ts
@@ -5,7 +5,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../../core/services/user.service';
 import { AuthService } from '../../../core/services/auth.service';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { OrderService } from '../../../core/services/order.service';
 
 @Component({
@@ -17,88 +17,96 @@ import { OrderService } from '../../../core/services/order.service';
 })
 export class PaymentComponent implements OnInit {
 
+  //On définis nos variables
   cartItems: cartItemInterface[] = [];
   user: UserInterface = {} as UserInterface;
-  delivery_price = 10;
+  delivery_price: number = 10;
   status = 'En attente de prise en charge';
   delivery: boolean = false;
   deliveryDate: string | null = null;
   paymentForm!: FormGroup;
+  isSubmitted = false;
 
+  //On injecte les services
   cartService = inject(CartService);
   userService = inject(UserService);
   authService = inject(AuthService);
   orderService = inject(OrderService);
-  fb = inject(FormBuilder);
   router = inject(Router);
 
+  //Méthode appelée lors de l'initialisation du composant
   ngOnInit(): void {
     if (this.isLogged()) {
       this.getUser();
     }
     this.loadCartItems();
-    this.initPaymentForm();
-  }
+    this.initializeForm();
+  };
 
-  private initPaymentForm() {
-    this.paymentForm = this.fb.group({
-      deliveryDate: [this.deliveryDate]
-    });
-    
-    this.paymentForm.get('deliveryDate')?.valueChanges.subscribe(value => {
-      this.deliveryDate = value;
-    });
-  }
+  //On récupère les items qui sont dans le localStorage
+  loadCartItems() {
+    const cartItemsString = localStorage.getItem('cartItems');
+    this.cartItems = cartItemsString ? JSON.parse(cartItemsString) : [];
+  };
+  
+  //On vérifie si le user est conncté
+  isLogged(): boolean {
+    return this.authService.isLogged();
+  };
 
-loadCartItems() {
-  const cartItemsString = localStorage.getItem('cartItems');
-  this.cartItems = cartItemsString ? JSON.parse(cartItemsString) : [];
-}
-
-  calculateTotalPrice(cartItem: cartItemInterface): number {
-    return cartItem.service.reduce((total, service) => total + service.price, cartItem.item.price) * cartItem.quantity;
-  }
-
-  subtotalPrice(): number {
-    return this.cartItems.reduce((total, item) => total + item.totalPrice, 0);
-  }
-
-  totalPrice(): number {
-    return this.subtotalPrice() + (this.delivery ? this.delivery_price : 0);
-  }
-
+  //On récupère les infos du user connecté
   getUser() {
     this.userService.getUserById().subscribe(data => {
       this.user = data;
     });
-  }
+  };
 
-  isLogged(): boolean {
-    return this.authService.isLogged();
-  }
+  //On initialise le formulaire
+  initializeForm() {
+    this.paymentForm = new FormGroup({
+      deliveryDate: new FormControl(null, [Validators.required])
+    });
+  };
 
+  // Fonctions pour calculer les prix
+  itemSubTotalPrice(cartItem: cartItemInterface): number {
+    return cartItem.service.reduce((total, service) => total + service.price, cartItem.item.price) * cartItem.quantity;
+  };
+  subtotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.totalPrice, 0);
+  };
+  totalPrice(): number {
+    return this.subtotalPrice() + (this.delivery ? this.delivery_price : 0);
+  };
+
+  //Fonction pour vérifier si on a choisis le livraion ou non
   onDeliveryChange(isDelivery: boolean) {
     this.delivery = isDelivery;
-    this.deliveryDate = isDelivery ? this.deliveryDate : null;
-    this.paymentForm.get('deliveryDate')?.setValue(this.deliveryDate);
-  }
+    if (!isDelivery) {
+      this.paymentForm.get('deliveryDate')?.reset();
+    }
+  };
   
-  payOrder() {
+  //Méthode appelée lors de la soumission du formulaire
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.delivery && this.paymentForm.invalid) {
+      return;
+    }
     const createdAt = new Date().toISOString();
-
     const newOrder: orderInterface = {
       price: this.totalPrice(),
       status: this.status,
       delivery: this.delivery,
-      deliveryDate: this.deliveryDate,
-      commandItems:this.cartItems,
+      deliveryDate: this.delivery ? this.paymentForm.get('deliveryDate')?.value : null,
+      commandItems: this.cartItems,
       createdAt: createdAt,
       user: this.user['@id'],
     };
-  
+
     this.orderService.createOrder(newOrder).subscribe(
       response => {
-        console.log('Commande crée', response);
+        console.log('Commande créée', response);
         localStorage.removeItem('cartItems');
         setTimeout(() => {
           location.reload();
@@ -109,7 +117,8 @@ loadCartItems() {
         console.error('Erreur lors de la création de la commande', error);
       }
     );
-  }
+  };
+
 }
 
 
