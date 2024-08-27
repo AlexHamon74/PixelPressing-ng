2e79c26e5c3dc37aa76d95c48e345721afc315f5
Alex Hamon <alex.hmn18@gmail.com>
2024-08-27 12:36:21 +0200

    update admin/dashboard

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SideNavAdminComponent } from '../../../shared/side-nav-admin/side-nav-admin.component';
+import { employeeInterface, orderInterface, UserInterface } from '../../../shared/entities';
+import { OrderService } from '../../../core/services/order.service';
+import { UserService } from '../../../core/services/user.service';
+import { EmployeeService } from '../../../core/services/employee.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [SideNavAdminComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css',
+  styleUrl: '../admin-style.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  orders: orderInterface[] = [];
+  users: UserInterface[] = [];
+  employees: employeeInterface[] = [];
+
+  totalClients: number = 0;
+  totalEmployees: number = 0;
+  completedOrders: number = 0;
+  ongoingOrders: number = 0;
+
+  orderService = inject(OrderService);
+  userService = inject(UserService);
+  employeeService = inject(EmployeeService);
+
+  ngOnInit(): void {
+    this.getOrders();
+    this.getUsers();
+    this.getEmployees();
+  };
+
+  getOrders() {
+    this.orderService.fetchAll().subscribe(data => {
+      this.orders = data;
+      this.completedOrders = this.orders.filter(order => order.status === 'Livré').length;
+      this.ongoingOrders = this.orders.filter(order => order.status !== 'Livré').length;
+    });
+  };
+
+  getUsers() {
+    this.userService.fetchAll().subscribe(data => {
+      this.users = data;
+      this.totalClients = this.users.length;
+    });
+  };
+
+  getEmployees() {
+    this.employeeService.fetchAll().subscribe(data => {
+      this.employees = data;
+      this.totalEmployees = this.employees.length;
+    });
+  };
 }
