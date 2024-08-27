46a4679407b5bbfc12cfe926e3e33f7c3b49fe6f
Alex Hamon <alex.hmn18@gmail.com>
2024-08-27 13:17:30 +0200

    update admin/order-edit

--- src/app/features/admin/orders/orders-edit/orders-edit.component.ts
+++ src/app/features/admin/orders/orders-edit/orders-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { SideNavAdminComponent } from '../../../../shared/side-nav-admin/side-nav-admin.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgFor } from '@angular/common';
-import { editOrderInterface, employeeInterface, newOrderInterface, orderInterface, UserInterface } from '../../../../shared/entities';
+import { editOrderInterface, employeeInterface, orderInterface } from '../../../../shared/entities';
 import { EmployeeService } from '../../../../core/services/employee.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrderService } from '../../../../core/services/order.service';
@@ -69,6 +69,12 @@ export class OrdersEditComponent implements OnInit {
     if (this.editOrderForm.valid && this.currentOrder) {
       const formValues = this.editOrderForm.value;
 
+      let deliveryDateConvert = null;
+        if (this.currentOrder.delivery) {
+          if (this.currentOrder.deliveryDate !== null) {
+              deliveryDateConvert = this.convertDate(this.currentOrder.deliveryDate);
+          }
+      }
       const createdAtConvert = this.convertDate(this.currentOrder.createdAt);
 
       const updatedOrder: editOrderInterface = {
@@ -76,13 +82,12 @@ export class OrdersEditComponent implements OnInit {
         price: this.currentOrder.price,
         status: formValues.status,
         delivery: this.currentOrder.delivery,
-        deliveryDate: this.currentOrder.deliveryDate,
+        deliveryDate: deliveryDateConvert,
         commandItems: this.currentOrder.commandItems,
         createdAt: createdAtConvert,
         user: this.currentOrder.user['@id'],
         employee: formValues.employee,
       };
-      console.log(updatedOrder)
       this.orderService.editOrder(updatedOrder).subscribe(() => {
         this.router.navigate(['/admin/order-list']);
       });
